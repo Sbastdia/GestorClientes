4b34b0f3831ba01db48946b255fd69f7a4fcb0b9
Sbastdia <sbastdia@myuax.com>
2022-09-30 12:14:50 +0200

    intentando arreglar fallos

--- Gestor_Clientes-main/run.py
+++ Gestor_Clientes-main/run.py
@@ -1,10 +1,11 @@
+from tkinter import Menu
 import ui
 import sys
 from gestor.menu import *
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "-t":
-        menu.iniciar()
+        Menu.iniciar()
     else:
         app = ui.MainWindow()
         app.mainloop()
